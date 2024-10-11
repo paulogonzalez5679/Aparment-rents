9c2296d06c6bb7fbe14bb9fb596c03a635caa711
sebastoledo009 <sebastoledo009@gmail.com>
2024-10-11 09:56:37 -0500

    Creacion de los componentes, codigo inicial en html y scss sin bootstrap

---
+++ src/app/pages/apartment-detail/apartment-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './apartment-detail.component.html',
+  styleUrl: './apartment-detail.component.scss'
+})
+export class ApartmentDetailComponent {
+
+}
